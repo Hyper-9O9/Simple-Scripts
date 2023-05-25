a72aebc6221174dc0e442cd31d3417f106b47e04
Hersh Hiwa <85962934+Hyper-9O9@users.noreply.github.com>
2023-05-25 15:31:16 +0300

    open_url.py

--- Web-Browser/open_url.py
+++ Web-Browser/open_url.py
@@ -6,7 +6,7 @@ chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
 # Register Chrome as the browser
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 # list urls I need to check
-url_list = ['vcenter.umm.org', 'vcsul.umm.org', '10.0.50.50', '10.0.50.55' , '172.26.0.5:5000', '172.26.1.1', '10.0.70.2', '10.0.1.20', '10.0.1.22' ]
+url_list = ["your urls go here" ]
 
 for url in url_list:
     # we use get chrome to be able to use google chrome, and add the urls we want as tabs
